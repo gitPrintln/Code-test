7e3166faf0aadd7621c3987cd0574782279ae9f9
gitPrintln <ghdcks101@naver.com>
2023-08-27 16:44:10 +0900

    공배수

---
+++ base/lv0/공배수.java
@@ -0,0 +1,9 @@
+package programmers;
+
+public class 공배수 {
+
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        return answer = number%n == 0 && number%m == 0 ? 1 : 0;
+    }
+}
